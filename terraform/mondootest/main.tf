c70a4ff9464642c844318aecfb89ef45e9a77e6c
codekan <66216396+codekan@users.noreply.github.com>
2025-03-22 23:35:18 +0100

    Update main.tf

--- terraform/mondootest/main.tf
+++ terraform/mondootest/main.tf
@@ -174,6 +174,26 @@ resource "azurerm_linux_virtual_machine" "linux_vm" {
   EOF
   )  
 }
+  #EXPERIMENTELLE COMMANDS
+  locals {
+    custom_data = <<CUSTOM_DATA
+    #!/bin/bash
+    sudo apt-get update
+    sudo apt-get install ufw
+    sudo ufw default allow outgoing
+    sudo ufw allow from 168.0.0.0/24 to any port 3389 proto tcp
+    sudo ufw enable
+    apt-get update
+    mkdir /tmp/TEEEEEEEEST
+    export MONDOO_REGISTRATION_TOKEN="${var.mondoo_token_linux}"
+    curl -sSL https://install.mondoo.com/sh | bash -s -- -u enable -s enable -t $MONDOO_REGISTRATION_TOKEN
+    cnspec scan local
+    CUSTOM_DATA
+  base64encode(local.custom_data)
+  }
+
+# Encode and pass you script
+custom_data = base64encode(local.custom_data)
 
 # Windows VM Setup
 
