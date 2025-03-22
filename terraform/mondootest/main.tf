e1245fc4e3c50abfb98c8b2ac93fed8da94b2fe4
codekan <66216396+codekan@users.noreply.github.com>
2025-03-23 00:47:08 +0100

    Update main.tf

--- terraform/mondootest/main.tf
+++ terraform/mondootest/main.tf
@@ -173,24 +173,29 @@ resource "azurerm_linux_virtual_machine" "linux_vm" {
   EOF
   )
 
-  #EXPERIMENTELLE COMMANDS
-  locals {
-    custom_data = <<CUSTOM_DATA
-    #!/bin/bash
-    sudo apt-get update
-    sudo apt-get install -y ufw
-    sudo ufw default allow outgoing
-    sudo ufw allow from 168.0.0.0/24 to any port 3389 proto tcp
-    sudo ufw enable
-    mkdir /tmp/TEEEEEEEEST
-    export MONDOO_REGISTRATION_TOKEN="${var.mondoo_token_linux}"
-    curl -sSL https://install.mondoo.com/sh | bash -s -- -u enable -s enable -t $MONDOO_REGISTRATION_TOKEN
-    cnspec scan local
-    CUSTOM_DATA
+  #EXPERIMENTELLE COMMANDS UND EXECUTION
+  variable "linux_shell_commands" {
+    default = <<-EOT
+      #!/bin/bash
+      sudo apt-get update -y
+      sudo apt-get install -y ufw 
+      sudo ufw default allow outgoing
+      sudo ufw allow from 168.0.0.0/24 to any port 3389 proto tcp
+      sudo ufw enable
+      mkdir /tmp/TEEEEEEEEST
+      export MONDOO_REGISTRATION_TOKEN="${var.mondoo_token_linux}"
+      curl -sSL https://install.mondoo.com/sh | bash -s -- -u enable -s enable -t $MONDOO_REGISTRATION_TOKEN
+      cnspec scan local  
+    EOT
   }
 
-  # Encode and pass the script from the locals stored as custom_data
-  custom_data = base64encode(local.custom_data)
+  settings = <<SETTINGS
+  {
+  "commandToExecute": "bash -c '${var.linux_shell_commands}'"
+  }
+  SETTINGS
+
+  }
 }
 
 
