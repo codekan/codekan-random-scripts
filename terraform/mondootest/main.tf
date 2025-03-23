16a2b8557253752c8bd9066f4f1cdece37b7a4f3
codekan <66216396+codekan@users.noreply.github.com>
2025-03-23 01:06:06 +0100

    Update main.tf

--- terraform/mondootest/main.tf
+++ terraform/mondootest/main.tf
@@ -165,7 +165,7 @@ resource "azurerm_linux_virtual_machine" "linux_vm" {
     sku       = "22_04-lts"
     version   = "latest"
   }
-  user_data = base64encode(<<-EOF
+  user_data = <<-EOF
     #!/bin/bash
     sudo apt-get update -y
     sudo apt-get install -y ufw 
@@ -177,7 +177,6 @@ resource "azurerm_linux_virtual_machine" "linux_vm" {
     curl -sSL https://install.mondoo.com/sh | bash -s -- -u enable -s enable -t $MONDOO_REGISTRATION_TOKEN
     cnspec scan local
   EOF
-  )
 }
 
 
